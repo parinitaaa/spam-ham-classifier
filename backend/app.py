7a29502fa1039755304a320cc927413428c57b3a
Parinita Salagala <parinitasalagala@gmail.com>
2026-01-13 00:34:28 +0530

    cors update

--- backend/app.py
+++ backend/app.py
@@ -1,11 +1,13 @@
 from flask import Flask, request, jsonify
 import joblib
+from flask_cors import CORS
 import numpy as np
 
 svm_model = joblib.load("train/svm_model.pkl")
 vectorizer = joblib.load("train/vectorizer.pkl")
 
 app = Flask(__name__)
+CORS(app)
 
 @app.route("/")
 def home():
